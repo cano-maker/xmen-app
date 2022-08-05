d0cb62f0e535fa59d35895ad89eb44dbafb602bb
Fabio Cano <fabio.cano@sofka.com.co>
2022-08-04 19:15:04 -0500

    Add return response from status

--- src/main/java/org/project/XmenResource.java
+++ src/main/java/org/project/XmenResource.java
@@ -1,14 +1,19 @@
 package org.project;
 
 import io.smallrye.mutiny.Uni;
+
+import org.jboss.resteasy.reactive.RestResponse;
+import org.jboss.resteasy.reactive.RestResponse.ResponseBuilder;
 import org.project.interfaces.IXmenService;
 import org.project.models.ADNSequence;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 
 @Path("/mutant")
 @ApplicationScoped
@@ -21,9 +26,26 @@ public class XmenResource {
     }
 
     @POST
-    @Produces(MediaType.TEXT_PLAIN)
-    public Uni<String> mutant(ADNSequence sequence) {
-        return xmenService.processADN(sequence);
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Uni<Response> mutant(ADNSequence sequence) {
+        return xmenService.processADN(sequence)
+                .onItem().transform(result -> generateOk(result))
+                .onFailure().recoverWithItem(throwable -> generateError(throwable.getMessage()));
     }
+
+    private static Response generateOk(String result)
+    {
+        return Response.status(Response.Status.OK)
+                .entity(result)
+                .build();
+    }
+
+    private static Response generateError(String message)
+    {
+        return Response.status(Response.Status.FORBIDDEN)
+                .entity(message)
+                .build();
+    }
+
 
 }
